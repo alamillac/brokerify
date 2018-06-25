cd96ddef672d0b712b397f5d37385e3b72ead473
Andres <andres@lamillac.com>
2018-06-25 21:25:59 +0200

    Eleconomista api

---
+++ app/api/eleconomista.py
@@ -0,0 +1,49 @@
+#!/bin/env python
+
+import requests
+import datetime
+from bs4 import BeautifulSoup
+from app.codes import IndexCodes
+
+AVAILABLE_INDEX = [IndexCodes.IBEX35]
+
+
+def str2float(value):
+    return float(value.replace(".","").replace(",","."))
+
+
+def api(index_name, start_date=datetime.date.today(), end_date=datetime.date.today()):
+    """
+    Get current values of index
+    """
+    def parse(response, index_name):
+        parser = BeautifulSoup(response, 'html.parser')
+        table = parser.find("table", {"class": "tablehistoricos"})
+        results = []
+        for row in table.findAll("tr")[2:]:
+            values = row.find("td")
+            if len(values) < 7:
+                break
+
+            date_raw, close_raw, var_raw, _, high_raw, low_raw, _ = [v.text for v in values]
+            close_val = str2float(close_raw)
+            var = str2float(var_raw)
+            open_val = close_raw - var
+            date = datetime.datetime.strptime(date_raw, "%d/%m/%y").date()
+            results.append({
+                "name": index_name,
+                "date": date,
+                "open": open_val,
+                "high": str2float(high_raw),
+                "low": str2float(low_raw),
+                "close": close_val
+            })
+        return results
+
+    if index_name not in AVAILABLE_INDEX:
+        raise Exception("Index not found")
+    if start_date > end_date:
+        raise Exception("Invalid date range")
+
+    url = "http://www.eleconomista.es/indice/IBEX-35/historico-fechas/{}/{}".format(start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
+    return parse(requests.get(url).text, index_name)
